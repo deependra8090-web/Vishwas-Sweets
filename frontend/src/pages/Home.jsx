e36af2632eea068f6d862ca6e72bdf62f2ee32e1
Deependra Yadav <deependra9433@gmail.com>
2026-06-13 00:02:54 +0530

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,13 @@ import Categories from "../components/Categories";
 import Hero from "../components/Hero";
 import Menus from "../components/Menus";
 import NewsLetter from "../components/NewsLetter";
-import Testimonial from "../components/Testimonial";
+
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Categories />
-      <Testimonial />
     </div>
   );
 };
